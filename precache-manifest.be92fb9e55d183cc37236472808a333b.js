4f18953da0ecc18c1cfd0a499a4e8341f22779ee
davidferro <david-ferro@sbcglobal.net>
2019-03-15 23:52:31 -0400

    Updates

--- precache-manifest.be92fb9e55d183cc37236472808a333b.js
+++ precache-manifest.be92fb9e55d183cc37236472808a333b.js
@@ -4,19 +4,19 @@ self.__precacheManifest = [
     "url": "/blackadder-clicky-game/static/js/runtime~main.684a1752.js"
   },
   {
-    "revision": "c3176aadcae6e5c2a127",
-    "url": "/blackadder-clicky-game/static/js/main.cc840a96.chunk.js"
+    "revision": "4f98eed4730e4e909910",
+    "url": "/blackadder-clicky-game/static/js/main.9ff6ea0a.chunk.js"
   },
   {
     "revision": "ba264cd90c4d36546d5a",
     "url": "/blackadder-clicky-game/static/js/2.8f71a45e.chunk.js"
   },
   {
-    "revision": "c3176aadcae6e5c2a127",
-    "url": "/blackadder-clicky-game/static/css/main.6b8008ba.chunk.css"
+    "revision": "4f98eed4730e4e909910",
+    "url": "/blackadder-clicky-game/static/css/main.7bd11f23.chunk.css"
   },
   {
-    "revision": "992ba3cd086fddd72ffaa8c192e24d37",
+    "revision": "1363c805205e3ad112e532542504cfba",
     "url": "/blackadder-clicky-game/index.html"
   }
 ];
